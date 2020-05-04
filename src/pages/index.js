9f513f35995c2da1c4bf1fa823df490846dc56be
mclarke <mclarkestudio@gmail.com>
2020-05-04 17:42:16 -0400

    removed css, barebones html

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,22 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Type from "../components/Type"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
+    <span>Matthew Clarke</span>
+  </div>
+  // <Layout>
+  //   <SEO title="Home" />
+  //   <h1>Hi people</h1>
+  //   <p>Welcome to your new Gatsby site.</p>
+  //   <p>Now go build something great.</p>
+  //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+  //     <Image />
+  //   </div>
+  // </Layout>
 )
 
 export default IndexPage
