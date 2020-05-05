319cee07a72a2ff7b7a452629bdb7373727863ad
mclarke <mclarkestudio@gmail.com>
2020-05-05 16:21:27 -0400

    article copy refactor

---
+++ src/copy/ArticleCopy.js
@@ -0,0 +1,22 @@
+export const ArticleCopy = [
+    {
+      heading: 'Liminal',
+      slug: 'A personal media library for the web.',
+      footer: '2020–On-going',
+    },
+    {
+      heading: 'Datavore Labs',
+      slug: 'The code-free tool for advanced time-series analysis.',
+      footer: '2020–On-going',
+    },
+    {
+      heading: 'Splashlight x Telmar',
+      slug: 'The highest standards in e-commerce photography and video.',
+      footer: '2020–On-going',
+    },
+    {
+      heading: 'Gagosian Gallery',
+      slug: 'Modern and contemporary art.',
+      footer: '2020-On-going.',
+    },
+  ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,29 +8,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Article from '../components/Article'
 import Type from "../components/Type"
-
-const ArticleCopy = [
-  {
-    heading: 'Liminal',
-    slug: 'A personal media library for the web.',
-    footer: '2020–On-going',
-  },
-  {
-    heading: 'Datavore Labs',
-    slug: 'The code-free tool for advanced time-series analysis.',
-    footer: '2020–On-going',
-  },
-  {
-    heading: 'Splashlight x Telmar',
-    slug: 'The highest standards in e-commerce photography and video.',
-    footer: '2020–On-going',
-  },
-  {
-    heading: 'Gagosian Gallery',
-    slug: 'Modern and contemporary art.',
-    footer: '2020-On-going.',
-  },
-]
+import { ArticleCopy } from '../copy/ArticleCopy'
 
 const IndexPage = () => {
 
