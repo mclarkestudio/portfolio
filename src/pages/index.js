3b44ad41b239bb4b02ee0c1c54e55c096813a508
mclarke <mclarkestudio@gmail.com>
2020-05-14 12:57:36 -0400

    image case sensitive netlify vuild fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/Image"
+import Image from "../components/image"
 import SEO from "../components/seo"
 import Article from "../components/Article"
 import Type from "../components/Type"
