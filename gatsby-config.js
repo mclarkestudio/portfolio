d8fe4eb5a0b0c8b54c726f414aed5bd7a88db3c2
mclarke <mclarkestudio@gmail.com>
2020-05-25 10:57:31 -0400

    some node mods removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     author: `Matthew Clarke`,
   },
   plugins: [
-    "gatsby-plugin-optimize-svgs",
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
@@ -79,23 +78,22 @@ module.exports = {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-W75HZJ7",
-  
+
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: true,
-  
+
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
         defaultDataLayer: { platform: "gatsby" },
-  
+
         // Specify optional GTM environment details.
         // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
         // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
         // dataLayerName: "YOUR_DATA_LAYER_NAME",
       },
     },
-
     // `gatsby-transformer-json`,
     // {
     //   resolve: `gatsby-source-filesystem`,
